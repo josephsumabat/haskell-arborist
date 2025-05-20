156d121bc79d09db41657b5f9fd21ea856276084
Joseph Sumabat <josephrsumabat@gmail.com>
2025-05-20 14:45:16 -0400

    mod file map test

--- test/Arborist/FilesSpec.hs
+++ test/Arborist/FilesSpec.hs
@@ -4,10 +4,19 @@ import TestImport
 import Test.Hspec
 import Arborist.Files
 import Control.Monad.IO.Class
+import Test.Hspec.Golden (defaultGolden)
+import qualified Data.HashMap.Strict as Map
+import Data.Text (Text)
+import qualified Data.Text as T
+import Hir.Parse
+import Hir.Types (ModuleText(..))
 
 spec :: Spec
 spec = do 
   describe "modFileMap" $ do
-    it "test" $ do
+    it "maps module names to their file paths" $ do
       mods <- buildModuleFileMap allTestLibs
-      pure ()
+      let modsText = T.unlines $ map (\(ModuleText {text}, path) -> 
+            T.concat [text, " -> ", T.pack path]) 
+            (Map.toList mods)
+      pure $ defaultGolden "buildModuleFileMap" (T.unpack modsText)
