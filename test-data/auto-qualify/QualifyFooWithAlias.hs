f1b52577a584974476f586f89936f95bd781e5ee
Shan Ali <shanali@Shans-MacBook-Pro.local>
2025-07-15 12:30:38 -0500

    feat: added testing for auto qualify

---
+++ test-data/auto-qualify/QualifyFooWithAlias.hs
@@ -0,0 +1,6 @@
+module QualifyFooWithAlias where
+
+import TestTypes qualified as T
+
+main :: Int
+main = foo 5
---
+++ test-data/auto-qualify/QualifyFooWithNoAlias.hs
@@ -0,0 +1,6 @@
+module QualifyFooWithAlias where
+
+import qualified TestTypes
+
+main :: Int
+main = foo 5
